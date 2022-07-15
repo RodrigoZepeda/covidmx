439f0c2f2ad469e13b57b462681e99ea16e8e218
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2022-07-15 10:48:21 -0500

    New zip

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,7 +2,7 @@ library(covidmx)
 
 covid      <- read_datos_abiertos()
 covid$dats <- covid$dats %>%
-  filter(FECHA_SINTOMAS >= as.Date("2020/05/01") & FECHA_SINTOMAS < as.Date("2020/07/01")) %>%
+  filter(FECHA_SINTOMAS >= as.Date("2022/05/01") & FECHA_SINTOMAS < as.Date("2020/07/01")) %>%
   filter(ENTIDAD_UM %in% c(18, 19))
 
 covid$dats   <- covid$dats %>% collect()
@@ -12,3 +12,5 @@ datosabiertos$disconnect <- function(){TRUE}
 
 usethis::use_data(datosabiertos, overwrite = TRUE, version = 3)
 datosabiertos$dats %>% write_excel_csv("pruebaCOVID19MEXICO.csv")
+zip("datos_abiertos_covid19.zip","pruebaCOVID19MEXICO.csv")
+file.remove("pruebaCOVID19MEXICO.csv")
