378e6f8db18a3a35b2a50471e76f4843cdff8ee6
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2022-07-15 15:28:58 -0500

    Changed zip

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,21 +1,35 @@
 library(covidmx)
 library(tidyverse)
 library(lubridate)
+
+#Leemos los datos
 covid      <- descarga_datos_abiertos(force_download = TRUE)
 
+#Filtramos BC y BCS para el ejemplo
 covid$dats <- covid$dats %>%
   filter(ENTIDAD_UM %in% c("02", "03"))
 
+#Obtenemos la info
 covid$dats   <- covid$dats %>% collect()
+
+#Limpiamos fecha de sintomas
 covid$dats   <- covid$dats %>%
   filter(year(FECHA_SINTOMAS) > 2020) %>%
   filter(FECHA_SINTOMAS >= ymd("2021/07/01") & FECHA_SINTOMAS <= ymd("2021/09/01"))
 
 covid$disconnect()
+
 datosabiertos <- covid
 datosabiertos$disconnect <- function(){TRUE}
 
+#Creamos los datos del paquete
 usethis::use_data(datosabiertos, overwrite = TRUE, version = 3)
-datosabiertos$dats %>% write_excel_csv("data-raw/pruebaCOVID19MEXICO.csv")
-zip("data-raw/datos_abiertos_covid19.zip","data-raw/pruebaCOVID19MEXICO.csv")
-file.remove("data-raw/pruebaCOVID19MEXICO.csv")
+
+#Almacenamos para generar el zip de testing
+datosabiertos$dats %>% write_excel_csv("pruebaCOVID19MEXICO.csv")
+zip("datos_abiertos_covid19.zip","pruebaCOVID19MEXICO.csv")
+
+#Reacomodamos todo el panorama
+file.remove("pruebaCOVID19MEXICO.csv")
+file.copy("datos_abiertos_covid19.zip", "data-raw/datos_abiertos_covid19.zip")
+file.remove("datos_abiertos_covid19.zip")
